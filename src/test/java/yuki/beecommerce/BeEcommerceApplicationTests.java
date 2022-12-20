cbf47343c4d748a9478fe6193e587bddfcbc8607
endlessmomo <soohyuk96@naver.com>
2022-12-20 12:24:48 +0900

    Initial commit

---
+++ src/test/java/yuki/beecommerce/BeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package yuki.beecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
